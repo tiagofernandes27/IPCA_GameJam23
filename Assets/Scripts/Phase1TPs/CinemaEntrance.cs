51716b0c721da08b313b719a75579a9fa3e24a38
Celestino <45713831+Cell21@users.noreply.github.com>
2023-05-27 20:56:24 +0100

    big push phase2

--- Assets/Scripts/Phase1TPs/CinemaEntrance.cs
+++ Assets/Scripts/Phase1TPs/CinemaEntrance.cs
@@ -7,6 +7,8 @@ public class CinemaEntrance : MonoBehaviour
     public PlayerMovement playerMovement;
     public GameObject Player;
     public GameObject InsideMarker;
+
+    //On collision enter isto poe o player do lado de fora do cinema num gameobject
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // Check if the collision is with a wall
