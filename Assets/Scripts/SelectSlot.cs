c0962a2e8480364d20e434f0cd1aae281ba270d7
DuarteMendonca24 <durtemendonca1234@gmail.com>
2023-05-27 23:14:44 +0100

    Inventory Hover

--- Assets/Scripts/SelectSlot.cs
+++ Assets/Scripts/SelectSlot.cs
@@ -2,26 +2,44 @@ using UnityEngine;
 
 public class SelectSlot : MonoBehaviour
 {
-    public GameObject[] slots;
-    private SpriteRenderer[] spriteRenderers;
+    private SpriteRenderer[] childSpriteRenderers;
+    private Color[] originalColors;
 
     void Start()
     {
-        slots = new GameObject[transform.childCount];
+        // Initialize the arrays with the correct size
+        childSpriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        originalColors = new Color[childSpriteRenderers.Length];
 
-        for (int i = 0; i < transform.childCount; i++)
+        // Save the original colors of the child objects' SpriteRenderers
+        for (int i = 0; i < childSpriteRenderers.Length; i++)
         {
-            slots[i] = transform.GetChild(i).gameObject;
-            spriteRenderers = slots[i].GetComponentsInChildren<SpriteRenderer>();
+            originalColors[i] = childSpriteRenderers[i].color;
         }
     }
 
-    private void OnMouseEnter()
+    void Update()
     {
-        // Change the color of each slot to the hover color
-        foreach (SpriteRenderer renderer in spriteRenderers)
+        // Cast a ray from the mouse position
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.GetRayIntersection(ray);
+
+        // Check if the ray hits a child object of the "Image" object
+        for (int i = 0; i < childSpriteRenderers.Length; i++)
         {
-            renderer.color = Color.green;
+            // Check if the current child object is being hovered over
+            if (hit && hit.transform.IsChildOf(transform) && hit.transform == childSpriteRenderers[i].transform)
+            {
+                // Change the color of the hovered child object
+                childSpriteRenderers[i].color = new Color(0.282f, 0.831f, 0.016f);
+            }
+            else
+            {
+                // Reset the color of non-hovered child objects
+                childSpriteRenderers[i].color = originalColors[i];
+            }
         }
     }
+
+   
 }
