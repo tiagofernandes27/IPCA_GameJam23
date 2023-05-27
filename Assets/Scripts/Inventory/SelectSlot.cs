4e1d8c9324a9c49462968de9e54435e1475c4e5c
Celestino <45713831+Cell21@users.noreply.github.com>
2023-05-27 22:05:14 +0100

    pushzinho

--- Assets/Scripts/Inventory/SelectSlot.cs
+++ Assets/Scripts/Inventory/SelectSlot.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SelectSlot : MonoBehaviour
+public class SelecionarSlot : MonoBehaviour
 {
     public GameObject[] slots;
    
